8e0f96c409c531642bbad2f394ec6c873b9c2f95
Anthony Suárez <neolight1010@gmail.com>
2021-08-18 02:48:59 -0500

    Complete protein_transltation from Python track.

---
+++ python/protein-translation/protein_translation.py
@@ -0,0 +1,43 @@
+from enum import Enum
+
+class Protein(Enum):
+    METHIONINE = "Methionine"
+    PHENYLALANINE = "Phenylalanine"
+    LEUCINE = "Leucine"
+    SERINE = "Serine"
+    CYSTEINE = "Cysteine"
+    TRYPTOPHAN = "Tryptophan"
+    TYROSINE = "Tyrosine"
+    STOP = "STOP"
+
+codon_proteins = {
+    "AUG": Protein.METHIONINE,
+    "UUU": Protein.PHENYLALANINE,
+    "UUC": Protein.PHENYLALANINE,
+    "UUA": Protein.LEUCINE,
+    "UUG": Protein.LEUCINE,
+    "UCU": Protein.SERINE,
+    "UCC": Protein.SERINE,
+    "UCA": Protein.SERINE,
+    "UCG": Protein.SERINE,
+    "UAU": Protein.TYROSINE,
+    "UAC": Protein.TYROSINE,
+    "UGU": Protein.CYSTEINE,
+    "UGC": Protein.CYSTEINE,
+    "UGG": Protein.TRYPTOPHAN,
+    "UAA": Protein.STOP,
+    "UAG": Protein.STOP,
+    "UGA": Protein.STOP,
+}
+
+def proteins(strand: str) -> list[str]:
+    result: list[str] = []
+
+    for i in range(0, len(strand), 3):
+        codon = strand[i : i+3]
+        translation = codon_proteins[codon]
+
+        if translation == Protein.STOP: break
+        result.append(translation.value)
+
+    return result
