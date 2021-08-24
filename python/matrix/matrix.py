b7a1077ac02f87c73a70164f837807c8b5cb261b
Anthony Suárez <neolight1010@gmail.com>
2021-08-24 14:51:41 -0500

    Improve matrix solution from Python track.

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -2,13 +2,13 @@ class Matrix:
     def __init__(self, matrix_string: str):
         row_strs = matrix_string.splitlines()
 
-        self.matrix: list[list[int]] = [[int(x) for x in row_str.split()] for row_str in row_strs]
+        self.matrix = [[int(x) for x in row_str.split()] for row_str in row_strs]
 
     def row(self, index: int) -> list[int]:
         """
         Return a row (1-base index).
         """
-        return self.matrix[index - 1]
+        return self.matrix[index - 1].copy()
 
     def column(self, index: int) -> list[int]:
         """
