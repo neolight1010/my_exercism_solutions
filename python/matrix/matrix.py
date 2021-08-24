23a93590fcb38f931e8b51db702dcf293c74cf18
Anthony Suárez <neolight1010@gmail.com>
2021-08-24 11:11:01 -0500

    Complete matrix problem from Python track.

---
+++ python/matrix/matrix.py
@@ -0,0 +1,17 @@
+class Matrix:
+    def __init__(self, matrix_string: str):
+        row_strs = matrix_string.splitlines()
+
+        self.matrix: list[list[int]] = [[int(x) for x in row_str.split()] for row_str in row_strs]
+
+    def row(self, index: int) -> list[int]:
+        """
+        Return a row (1-base index).
+        """
+        return self.matrix[index - 1]
+
+    def column(self, index: int) -> list[int]:
+        """
+        Return a column (1-base index).
+        """
+        return [row[index - 1] for row in self.matrix]
