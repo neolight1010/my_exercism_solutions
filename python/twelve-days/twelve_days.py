b5a41a4b05b4a9668bbe455f833785c04a428df7
Anthony Suárez <neolight1010@gmail.com>
2022-03-04 21:27:12 -0500

    Improve twelve_days: Python.

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,42 +1,57 @@
-def first_sentence(day: int) -> str:
-    ordinal_numbers = ["first", "second", "third", "fourth", "fifth",
-                       "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"]
+ORDINAL_NUMBERS = ["first", "second", "third", "fourth", "fifth",
+                   "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"]
 
-    return f"On the {ordinal_numbers[day - 1]} day of Christmas my true love gave to me: "
+
+GIFTS = [
+    "a Partridge in a Pear Tree",
+    "two Turtle Doves",
+    "three French Hens",
+    "four Calling Birds",
+    "five Gold Rings",
+    "six Geese-a-Laying",
+    "seven Swans-a-Swimming",
+    "eight Maids-a-Milking",
+    "nine Ladies Dancing",
+    "ten Lords-a-Leaping",
+    "eleven Pipers Piping",
+    "twelve Drummers Drumming"
+]
+
+
+def validate_day(day: int) -> None:
+    if day < 1 or day > 12:
+        raise ValueError("day number must be an integer from 1 to 12.")
+
+
+def first_sentence(day: int) -> str:
+    validate_day(day)
+
+    return f"On the {ORDINAL_NUMBERS[day - 1]} day of Christmas my true love gave to me: "
+
+
+def gift_list_sentence(day: int) -> str:
+    validate_day(day)
+
+    day_i = day - 1
+    sentence = GIFTS[day_i]
+
+    if day_i == 0:
+        return sentence + "."
+
+    separator = ", " if day_i > 1 else ", and "
+
+    return sentence + separator + gift_list_sentence(day - 1)
 
 
 def recite(start_verse: int, end_verse: int) -> list[str]:
     if start_verse > end_verse:
         raise ValueError("start_verse must be less than or equal to end_verse.")
 
-    if start_verse < 1:
-        raise ValueError("start_verse must be an integer from 1 to 12.")
-
-    if end_verse > 12:
-        raise ValueError("end_verse must be an integer from 1 to 12.")
-
-    gifts = [
-        "a Partridge in a Pear Tree",
-        "two Turtle Doves",
-        "three French Hens",
-        "four Calling Birds",
-        "five Gold Rings",
-        "six Geese-a-Laying",
-        "seven Swans-a-Swimming",
-        "eight Maids-a-Milking",
-        "nine Ladies Dancing",
-        "ten Lords-a-Leaping",
-        "eleven Pipers Piping",
-        "twelve Drummers Drumming"
-    ]
+    validate_day(start_verse)
+    validate_day(end_verse)
 
     verse = first_sentence(start_verse)
-
-    if start_verse > 1:
-        verse += ", ".join(gifts[1:start_verse][::-1])
-        verse += f", and {gifts[0]}."
-    else:
-        verse += gifts[0] + "."
+    verse += gift_list_sentence(start_verse)
 
     result = [verse]
 
