63b01309879d53eef6cc56bc48e824974f7440a5
Anthony Suárez <neolight1010@gmail.com>
2022-03-05 20:20:19 -0500

    Complete lucians-luscious-lasagna: Haskell.

---
+++ haskell/lucians-luscious-lasagna/src/LuciansLusciousLasagna.hs
@@ -0,0 +1,11 @@
+module LuciansLusciousLasagna (elapsedTimeInMinutes, expectedMinutesInOven, preparationTimeInMinutes) where
+
+expectedMinutesInOven :: Int
+expectedMinutesInOven = 40
+
+preparationTimeInMinutes :: Int -> Int
+preparationTimeInMinutes = (*) 2
+
+elapsedTimeInMinutes :: Int -> Int -> Int
+elapsedTimeInMinutes layers minutesInOven =
+  preparationTimeInMinutes layers + minutesInOven
