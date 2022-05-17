c8912f1395d084c2439baae7604d9ee4023afa49
Anthony Suárez <neolight1010@gmail.com>
2022-05-16 19:50:06 -0500

    Haskell: Try foldMap solution for nucleotideCounts.

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -2,9 +2,10 @@
 
 module DNA (nucleotideCounts, Nucleotide (..)) where
 
-import Control.Monad (foldM)
+import Control.Monad (foldM, (>=>))
 import Data.Map (Map, insertWith)
 import qualified Data.Map as Map
+import Numeric.Natural (Natural)
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
@@ -21,3 +22,32 @@ toNucleotide = \case
   'G' -> Right G
   'T' -> Right T
   _ -> Left "Invalid nucleotide."
+
+--------------------------
+
+data NucleotideCounts = NucleotideCounts
+  { getAs :: Natural,
+    getCs :: Natural,
+    getGs :: Natural,
+    getTs :: Natural
+  }
+  deriving (Show)
+
+instance Monoid NucleotideCounts where
+  mempty = NucleotideCounts 0 0 0 0
+
+instance Semigroup NucleotideCounts where
+  (<>) a b =
+    NucleotideCounts
+      { getAs = getAs a + getAs b,
+        getCs = getCs a + getCs b,
+        getGs = getGs a + getGs b,
+        getTs = getTs a + getTs b
+      }
+
+nucleotideToNucleotideCounts :: Nucleotide -> NucleotideCounts
+nucleotideToNucleotideCounts n = case n of
+  A -> mempty {getAs = 1}
+  C -> mempty {getCs = 1}
+  G -> mempty {getGs = 1}
+  T -> mempty {getTs = 1}
