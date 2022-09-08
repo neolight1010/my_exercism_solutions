0ca7d243d5ad6803d4d42f9da592f26b046aded2
Anthony Suárez <neolight1010@gmail.com>
2022-09-07 22:56:51 -0500

    Haskell: Complete Clock

--- haskell/clock/src/Clock.hs
+++ haskell/clock/src/Clock.hs
@@ -1,13 +1,35 @@
 module Clock (addDelta, fromHourMin, toString) where
 
-data Clock = Dummy
-  deriving Eq
+data Clock = Clock
+  { clockHour :: Int,
+    clockMinute :: Int
+  }
+  deriving (Eq, Show)
 
 fromHourMin :: Int -> Int -> Clock
-fromHourMin hour min = error "You need to implement this function."
+fromHourMin hour minute =
+  Clock
+    { clockHour = dayHour,
+      clockMinute = dayMinute
+    }
+  where
+    (extraHoursInMinutes, dayMinute) = divMod minute 60
+    dayHour = (hour + extraHoursInMinutes) `mod` 24
 
 toString :: Clock -> String
-toString clock = error "You need to implement this function."
+toString clock = hourStr ++ ":" ++ minuteStr
+  where
+    hourStr = showIntWithLeadingZero (clockHour clock)
+    minuteStr = showIntWithLeadingZero (clockMinute clock)
+
+    showIntWithLeadingZero :: Int -> String
+    showIntWithLeadingZero n =
+      if n < 10
+        then "0" ++ show n
+        else show n
 
 addDelta :: Int -> Int -> Clock -> Clock
-addDelta hour min clock = error "You need to implement this function."
+addDelta hours minutes clock = fromHourMin totalHours totalMinutes
+  where
+    totalHours = clockHour clock + hours
+    totalMinutes = clockMinute clock + minutes
