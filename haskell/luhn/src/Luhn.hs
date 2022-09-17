f80738deb943999be6a8fe95d3f1aa480b4ee44f
Anthony Suárez <neolight1010@gmail.com>
2022-09-17 09:39:30 -0500

    Haskell: Complete Luhn

--- haskell/luhn/src/Luhn.hs
+++ haskell/luhn/src/Luhn.hs
@@ -1,4 +1,27 @@
 module Luhn (isValid) where
 
 isValid :: String -> Bool
-isValid n = error "You need to implement this function."
+isValid xs
+  | length normalized <= 1 = False
+  | otherwise = validateDigits . getDigits $ normalized
+  where
+    normalized = normalize xs
+
+normalize :: String -> String
+normalize = filter (/= ' ')
+
+getDigits :: String -> [Int]
+getDigits = map (read . (: ""))
+
+validateDigits :: [Int] -> Bool
+validateDigits = (== 0) . (`mod` 10) . sum . transformDigits
+
+transformDigits :: [Int] -> [Int]
+transformDigits = zipWith transformDigit [0 :: Int ..] . reverse
+
+transformDigit :: Int -> Int -> Int
+transformDigit i x
+  | odd i = reduceIfLarge . (* 2) $ x
+  | otherwise = x
+  where
+    reduceIfLarge n = if n > 9 then n - 9 else n
