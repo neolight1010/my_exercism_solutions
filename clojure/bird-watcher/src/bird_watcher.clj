fd3ff61d60d058c5dde8b8e84aeac9b3d652b209
Anthony Suárez <neolight1010@gmail.com>
2023-03-26 09:23:58 -0500

    clojure: Complete Bird Watcher

--- clojure/bird-watcher/src/bird_watcher.clj
+++ clojure/bird-watcher/src/bird_watcher.clj
@@ -1,25 +1,22 @@
 (ns bird-watcher)
 
 (def last-week 
-  (vector 0 2 5 3 7 8 4))
+  [0 2 5 3 7 8 4])
 
-(defn today [birds]
-  (last birds))
+(def today last)
 
 (defn inc-bird [birds]
   (conj (pop birds)  (+ 1 (today birds))))
 
 (defn day-without-birds? [birds]
-  (boolean (some #(= 0 %) birds)))
+  (not (every? pos? birds)))
 
 (defn n-days-count [birds n]
   (reduce + (take n birds)))
 
 (defn busy-days [birds]
-  (count (filterv #(>= % 5) birds)))
+  (count (filter #(>= % 5) birds)))
 
 (defn odd-week? [birds]
-  (cond
-    (= (vector 1 0 1 0 1 0 1) birds) true
-    (= (vector 0 1 0 1 0 1 0) birds) true
-    :else false))
+  (let [odd-week (take (count birds) (cycle '(1 0)))]
+    (= odd-week birds)))
