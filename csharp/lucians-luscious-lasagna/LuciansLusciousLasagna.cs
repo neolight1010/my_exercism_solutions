e60f9196195252c168a52d8c0090f668234dc8cc
Anthony Suárez <neolight1010@gmail.com>
2023-07-25 21:07:25 -0500

    csharp: Complete Lucian's Luscious Lasagna

---
+++ csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -0,0 +1,12 @@
+class Lasagna
+{
+    public int ExpectedMinutesInOven() => 40;
+
+    public int RemainingMinutesInOven(int minutesInOven) =>
+        ExpectedMinutesInOven() - minutesInOven;
+
+    public int PreparationTimeInMinutes(int layers) => layers * 2;
+
+    public int ElapsedTimeInMinutes(int layers, int minutesInOven) =>
+        minutesInOven + PreparationTimeInMinutes(layers);
+}
